41a4929b38d5be23a76806dcea41ab7b9e548e8b
Marisa Tania <60201466+mt-cs@users.noreply.github.com>
2020-06-05 17:20:30 -0700

    Create DiscountBill.java
    BJP4 Exercise 9.10: DiscountBill

---
+++ DiscountBill.java
@@ -0,0 +1,50 @@
+public class DiscountBill extends GroceryBill {
+    private boolean preferred;
+    private int discountItem;
+    private double discount;
+
+    public DiscountBill(Employee clerk, boolean preferred) {
+        super(clerk);
+        this.preferred = preferred;
+        discount=0.0;
+        discountItem=0;
+    }
+
+    @Override
+    public void add(Item i){
+        super.add(i);
+        if(preferred && i.getDiscount()>0){
+            discountItem++;
+            discount += i.getDiscount();
+        }
+    }
+    
+    /**
+     * returns the number of items that were discounted, if any
+     * @return discountItem
+     */
+    public int getDiscountCount(){
+        return discountItem;
+    }
+
+    /**
+     * returns the total discount for this list of items, if any
+     * @return discount
+     */
+    public double getDiscountAmount(){
+        return discount;
+    }
+
+    /**
+     * returns the percent of the total discount as a percent of what the total would have been otherwise
+     * @return discount
+     */
+    public double getDiscountPercent(){
+        return discount*100/super.getTotal();
+    }
+
+    @Override
+    public double getTotal(){
+        return super.getTotal()-discount;
+    }
+}
